1a340b7d2ea7afc44af0a341b9e9f23f3f1cd45b
Eric Harrison <magicshadow818@gmail.com>
2024-02-29 00:20:36 +0000

    Save progress: 50.1

--- dumpDatabase.sh
+++ dumpDatabase.sh
@@ -1,4 +1,4 @@
-pg_dump -cC --inserts -U freecodecamp mario_database > mario_database.sql
+pg_dump -cC --inserts -U freecodecamp students > students.sql
 git add .
 git commit -m "saving dump of database..."
 git push origin master
--- rebuildDatabase.sh
+++ rebuildDatabase.sh
@@ -1,2 +1,2 @@
-psql -U postgres < mario_database.sql
+psql -U postgres < students.sql
 psql --username=freecodecamp --dbname=postgres
